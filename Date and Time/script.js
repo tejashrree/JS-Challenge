6fba2b9006ef6dadfcb345b23e65a4e27c8971a9
Tejashree Trivedi <156522843+tejashrree@users.noreply.github.com>
2026-03-26 10:12:55 +0530

    Add files via upload

---
+++ Date and Time/script.js
@@ -0,0 +1,10 @@
+// Program-1
+let currentDate = new Date();
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+console.log("Today is : " + days[currentDate.getDay()]);
+console.log("Current Time is : " + currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds());
+console.log(currentDate);
+
+
+//Program-2
+console.log(printCurrentPge());
