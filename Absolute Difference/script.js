278a318064735609206c0048bcae2892e0454c76
Tejashree Trivedi <156522843+tejashrree@users.noreply.github.com>
2026-03-31 10:04:35 +0530

    Add files via upload

---
+++ Absolute Difference/script.js
@@ -0,0 +1,4 @@
+let num1 = Number(prompt("Enter first number :"));
+let num2 = Number(prompt("Enter seecond number :"));
+let result = Math.abs(num1-num2);
+console.log(result);
---
+++ Array length same or not/script.js
@@ -0,0 +1,8 @@
+function equalLength(arr1, arr2) {
+    if (arr1.length === arr2.length || (arr1.substring(0, i - 1)) === (arr2.substring(0, i - 1))) {
+        console.log("Same length");
+    }
+    return true
+}
+
+console.log(equalLength([1,2,3,4],[1,2,3,4]));
