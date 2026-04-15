7ecdeb371fd09b51e10e81362e90a09a9dac515b
Tejashree Trivedi <156522843+tejashrree@users.noreply.github.com>
2026-04-15 10:17:24 +0530

    Add files via upload

---
+++ Form new arr from middle elements of 2 arrays/script.js
@@ -0,0 +1,6 @@
+let arr1 = [0, 1, 2];
+let arr2 = [10, 20, 30];
+let result1 = arr1[1];
+let result2 = arr2[1];
+let outcome =[].concat(result1,result2);
+console.log(outcome); 
---
+++ Form new array from first and last digit of same array/script.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, 3, 4];
+let first = arr[0];
+let last = arr.length;
+let result = [].concat(first, last);
+console.log(result);
